e0fd8cf33d556883c5f36e16efb2c716b97383c6
NecroKote <1721257+NecroKote@users.noreply.github.com>
2026-06-03 11:53:19 +0200

    add watchdog for consumer
    since Saleryd doen't support PING/PONG this is the only reliable way to detect disconnects.
    It can be disabled by setting `recv_timeout` to `None`

--- src/pysaleryd_tage/client.py
+++ src/pysaleryd_tage/client.py
@@ -7,31 +7,53 @@ from .device.device import SalerydDevice
 
 
 @asynccontextmanager
-async def connected_device(host: str, port: int = 3001):
+async def connected_device(
+    host: str,
+    port: int = 3001,
+    recv_timeout: float | None = 4.0,
+):
     device = SalerydDevice()
     disconnected = asyncio.Event()
 
-    async def _consumer(ws):
+    async def _consumer(ws, event: asyncio.Event):
         async for message in ws:
+            event.set()
             await device.on_receive(message)
+
         disconnected.set()
 
     async def _producer(ws):
         await device.process(ws.send)
+
+    async def _consumer_watchdog(ws, event: asyncio.Event):
+        while True:
+            event.clear()
+            await asyncio.sleep(recv_timeout)
+            if not event.is_set():
+                break
+
+        disconnected.set()
 
     async with connect(
         f"ws://{host}:{port}",
         ping_interval=None,
         ping_timeout=None,
     ) as ws:
-        consumer_task = asyncio.create_task(_consumer(ws))
-        producer_task = asyncio.create_task(_producer(ws))
+        got_packet_event = asyncio.Event()
+
+        tasks = [
+            asyncio.create_task(_consumer(ws, got_packet_event)),
+            asyncio.create_task(_producer(ws)),
+        ]
+
+        if recv_timeout is not None:
+            tasks.append(asyncio.create_task(_consumer_watchdog(ws, got_packet_event)))
 
         try:
-            await asyncio.wait_for(device.is_ready.wait(), timeout=5)
+            await asyncio.wait_for(device.is_ready.wait(), timeout=recv_timeout)
             yield device, disconnected
 
         finally:
-            consumer_task.cancel()
-            producer_task.cancel()
-            await asyncio.gather(consumer_task, producer_task, return_exceptions=True)
+            for task in tasks:
+                task.cancel()
+            await asyncio.gather(*tasks, return_exceptions=True)
